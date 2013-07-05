c65dcdbbf312408449704968b40c906e30912ff9
rmfbarker <richbarker@gmail.com>
2013-07-05 21:16:23 +1000

    Update to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+app.get('/', function (request, response) {
+    var result = fs.readFileSync('index.html').toString();
+    response.send(result);
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
