496e64a4be294f35e41b08d29e735585e6c52bbb
rmfbarker <richbarker@gmail.com>
2013-07-07 16:10:31 +1000

    Grader now reads files from remote Urls as well as local

--- grader.js
+++ grader.js
@@ -21,13 +21,16 @@
  - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
  */
 
-var fs = require('fs');
-var program = require('commander');
-var cheerio = require('cheerio');
+var fs = require('fs'),
+    program = require('commander'),
+    cheerio = require('cheerio'),
+    rest = require('restler'),
+    sys = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var assertFileExists = function(infile) {
+var assertFileExists = function (infile) {
     var instr = infile.toString();
     if (!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
@@ -36,15 +39,15 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlmFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlmFile = function (htmlfile) {
+    return cheerio.load(htmlfile);
 };
 
-var loadChecks = function(checksfile) {
+var loadChecks = function (checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function (htmlfile, checksfile) {
     var $ = cheerioHtmlmFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -55,20 +58,37 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var clone = function(fn) {
+var clone = function (fn) {
     // workaround for commander.js issue.
     //http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
-if(require.main == module) {
+if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var checkJson, outJson, content;
+    if (program.url) {
+        rest.get(program.url).on('complete', function (result) {
+            if (result instanceof Error) {
+                sys.puts('Error: ' + result.message);
+                this.retry(5000); // try again after 5 sec
+            } else {
+                checkJson = checkHtmlFile(result, program.checks);
+                outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            }
+        });
+    } else {
+        content = fs.readFileSync(program.file);
+        checkJson = checkHtmlFile(content, program.checks);
+        outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
